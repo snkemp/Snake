0d3b63d9dad632b14f5e1f5a339577da4fa4e8bf
Spencer Kemp <snkemp@stetson.edu>
2018-03-29 14:18:22 -0400

    Another commit

--- index.js
+++ index.js
@@ -3,6 +3,29 @@
  * snkemp
 **/
 
+var container, renderer, scene, camera, control, animating, gui;
+
 function onload() {
 
+    /* Needed components */
+    container = document.getElementById('container');
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize( window.offsetWidth, window.offsetHeight );
+    container.appendChild( renderer.domElement );
+
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 1, 10000 );
+    scene = new THREE.Scene();
+    scene.add(camera);
+
+    control = new THREE.OrbitControls( camera, document, renderer.domElement );
+
+    window.addEventListener( 'resize', function() {
+        camera.aspect = window.innerWidth/window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth/window.innerHeight );
+    }, false );
+
+
+    gui = new dat.GUI();
 }
